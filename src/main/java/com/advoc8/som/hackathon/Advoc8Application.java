816d74a982af70227a592489368ee2bffff06c32
Amin Yuddin <mohdaminyuddin@gmail.com>
2017-07-29 11:30:39 +0800

    Add an empty UI admin template

---
+++ src/main/java/com/advoc8/som/hackathon/Advoc8Application.java
@@ -0,0 +1,12 @@
+package com.advoc8.som.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Advoc8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Advoc8Application.class, args);
+	}
+}
---
+++ src/main/java/com/advoc8/som/hackathon/configuration/Advoc8Configuration.java
@@ -0,0 +1,8 @@
+package com.advoc8.som.hackathon.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Advoc8Configuration {
+	
+}
